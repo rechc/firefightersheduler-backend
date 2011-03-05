26ae988e019866afdac660834de25b1312d017b6
rech.christian <rech.christian@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-05 16:49:37 +0000

    Benutzer hinzufügen grober view erstellt

---
+++ trunk/FFS/Controller/createUser.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Description
+ *
+ *@author Rech Christian
+ *@version alpha
+ */
+    require_once '../Model/User.php';
+
+    $user = new User();
+    $user->setName($_POST['firstname']);
+    $user->setVorname($_POST['lastname']);
+    $user->setEmail($_POST['email']);
+    $user->setGebDat($_POST['bday']);
+    $user->setPassword("4711");
+    $user->setRollen_ID(4);
+
+    $user->create_db_entry();
+
+    header("Location: usersOverview.php");
+?>
