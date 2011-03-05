17b6697d099dc32a325f07e5ad2e3be7234fb8d8
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-05 11:42:01 +0000

    nicht AGTler (agt = 0) aus Uebersicht ausgeschlossen

--- FFS/Model/AllUser.php
+++ FFS/Model/AllUser.php
@@ -15,15 +15,16 @@
      * @return User-ArrayObject
      */
      public static function get_userarray_for_manager_view(){
-         $sql = "SELECT ID FROM user";
+         $sql = "SELECT ID FROM user WHERE agt=1";
 
          $dbConnector = DbConnector::getInstance();
          $result = $dbConnector->execute_sql($sql);
 
          $user_array = new ArrayObject();
          while($data = mysql_fetch_array($result)){
-             $user = User::get_user(2);  //WICHTIG 1 muss zu $data["ID"]
-             $user_array->append($user);
+            $user = User::get_user($data["ID"]);
+            //$user = User::get_user(2);  //verwenden wenn es Probleme gibt. Zum testen
+            $user_array->append($user);
          }
          return $user_array;
      }
