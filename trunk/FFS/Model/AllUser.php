7b41bbb1d4d0b1b4ef463eeceb3cff6aff56b7dd
WarkenA <WarkenA@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-05 13:30:28 +0000

    viewuser optimiert

--- trunk/FFS/Model/AllUser.php
+++ trunk/FFS/Model/AllUser.php
@@ -15,7 +15,7 @@
      * @return User-ArrayObject
      */
      public static function get_userarray_for_manager_view(){
-         $sql = "SELECT ID FROM user WHERE agt=1";
+         $sql = "SELECT ID FROM user WHERE agt=1"; // später fixen 
 
          $dbConnector = DbConnector::getInstance();
          $result = $dbConnector->execute_sql($sql);
