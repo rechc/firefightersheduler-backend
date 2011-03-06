a827de2cdef510f1ed1f6af56d8fb16dc106d7fb
WarkenA <WarkenA@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-06 19:11:43 +0000

    benutzeruebersicht

--- FFS/Model/EinsatzListe.php
+++ FFS/Model/EinsatzListe.php
@@ -116,7 +116,7 @@ class EinsatzListe {
      * (geht von einer sortierten Liste aus)
      */
     public function get_warning_status() {
-        if ($this->einsatz_array[0] == NULL){return Config::red();}
+       // //TODO notice vllt so weg if ($this->einsatz_array == NULL){return Config::red();}
         if (is_a($this->einsatz_array[0], "Einsatz")) {
             return $this->einsatz_array[0]->get_warning_status();
         } else {
