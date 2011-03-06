eff700b17a3f806627ea3be15efa2bec051ee780
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-06 18:43:12 +0000

    G26 updaten und neu einfuegen

--- FFS/View/editG26.php
+++ FFS/View/editG26.php
@@ -8,18 +8,32 @@
 ?>
     <div id="content">
         <?php
-            if (isset($_REQUEST["uid"]))
-                        $uid = $_REQUEST["uid"];
-                    else
-                        $uid = $_SESSION["user_id"];
-            $user=User::get_user($uid);
+            $uid = $_REQUEST["uid"];            
             $g26id  = $_GET["id"];
             $g26    = G26::load_by_g26id($g26id);
-            $datum  = $g26->getDatum();
-            $gueltig= $g26->getGueltigBis();
+            if (isset($_POST["aendern"])) {
+                $g26->setDatum($_POST["datum"]);
+                $g26->setGueltigBis($_POST["gueltigBis"]);
+                $g26->save();
+                $datum  = $g26->getDatum();
+                $gueltig= $g26->getGueltigBis();
+            } elseif (isset($_POST["neu"])) {
+                $g26 = new G26();
+                $g26->setUserID($uid);
+                $g26->setDatum($_POST["datum"]);
+                $g26->setGueltigBis($_POST["gueltigBis"]);
+                $g26->create_db_entry();
+                $datum  = $g26->getDatum();
+                $gueltig= $g26->getGueltigBis();     
+            } else {
+                $datum  = $g26->getDatum();
+                $gueltig= $g26->getGueltigBis();
+            }
+            $user=User::get_user($uid);
+
         ?>
         <div><h1>Erfasste Untersuchung ändern</h1></div>
-        <form action="../Model/Authentification/login.php" method="post">
+        <form action="" method="post">
             <div>
                 <table>
                     <tr>
@@ -32,7 +46,8 @@
                     </tr>
                     <tr>
                         <td><input type="hidden" name="id" value="<?php echo $g26id ?>">&nbsp;</td>
-                        <td><input type="submit" value="Änderungen speichern"></td>
+                        <td><input type="submit" name="aendern" value="Änderungen speichern">&nbsp;
+                            <input type="submit" name="neu" value="Neuer Datensatz"></td>
                     </tr>
                 </table>
             </div>
@@ -45,7 +60,7 @@
                     <th width="150px">Datum</th>
                     <th>G&uuml;ltig bis</th>
                     <?php
-                        if ($_SESSION["user_rolle"] == 40 | $_SESSION["user_id"] == 50) {
+                        if ($_SESSION["user_rolle"] == 40 | $_SESSION["user_rolle"] == 50) {
                             echo "<th class=\"manage\">&nbsp;</th>\n";
                         }
                     ?>
@@ -58,26 +73,6 @@
                 ?>
             </tbody>
         </table>
-        <br /><hr>
-        <div><h1>neue Untersuchung erfassen</h1></div>
-        <form action="../Model/Authentification/login.php" method="post">
-            <div>
-                <table>
-                    <tr>
-                        <td>Datum:</td>
-                        <td><input type="text" name="datum"></td>
-                    </tr>
-                    <tr>
-                        <td>G&uuml;ltig bis:</td>
-                        <td><input type="text" name="gueltigBis"></td>
-                    </tr>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td><input type="submit" value="Untersuchung erfassen"></td>
-                    </tr>
-                </table>
-            </div>
-        </form>
     <!-- Ende Content -->
     </div>
 <?php include (PATH_BASIS . '/View/Layout/footer.inc.php') ?>
