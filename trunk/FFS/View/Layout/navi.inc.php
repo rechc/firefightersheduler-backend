ab03eff8e6a9def660db7cfee16df0396db682c7
rech.christian <rech.christian@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-06 18:15:39 +0000

    bla bla

--- trunk/FFS/View/Layout/navi.inc.php
+++ trunk/FFS/View/Layout/navi.inc.php
@@ -1,7 +1,7 @@
     <div id="buttonbar">
         <ul>
           <!-- <li><a href="index.php">Startseite</a></li> -->
-          <li><a href="userOverview.php">Benutzer Übersicht</a></li>
+          <li><a href="userOverview.php">Benutzerübersicht</a></li>
           <li><a href="viewUser.php">Meine Daten</a></li>
           <li><a href="addData.php">Daten hinzuf&uuml;gen</a></li>
           <!-- <li><a href="login.php">Login</a></li> -->
