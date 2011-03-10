4fbad5ea66821a0d2b058ab9188a19b24f979fd7
rech.christian <rech.christian@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-10 18:25:56 +0000

    kleingram

---
+++ FFS/Controller/DeleteUser.php
@@ -0,0 +1,8 @@
+<?php
+    require_once PATH_BASIS . '/Model/User.php';
+    require_once PATH_BASIS .'/Controller/Authentification/authorizationCheck.php';
+
+    $id = $_REQUEST["uid"];
+    $user = User::get_user($id);
+    $user->delete_with_dependencys();
+?>
