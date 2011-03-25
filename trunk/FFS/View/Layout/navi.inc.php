68afd575942bda92f4491490bca7258ed3fc3507
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-25 16:19:34 +0000

    Navi funzt jetzt. 200 Puls. Bald. Dooohhhhh

--- trunk/FFS/View/Layout/navi.inc.php
+++ trunk/FFS/View/Layout/navi.inc.php
@@ -1,14 +1,14 @@
     <?php 
     include_once PATH_BASIS . '/Controller/SessionHelper.php';
     if(SessionHelper::isLoggedIn()){
-        echo "<div id='buttonbar'>
+        echo "<div id=\"buttonbar\">
         <ul>
-          <li><a href='viewUser.php'>Meine Daten</a></li>";
+          <li><a href=\"viewUser.php\">Meine Daten</a></li>";
           if(SessionHelper::isAdminOrAGW()){
-            echo "<li><a href='userOverview.php'>Benutzerübersicht</a></li>";
-            echo "<li><a href='addData.php'>Daten hinzuf&uuml;gen</a></li>";
+            echo "<li><a href=\"userOverview.php\">Benutzerübersicht</a></li>";
+            echo "<li><a href=\"addData.php\">Daten hinzuf&uuml;gen</a></li>";
           }
-          echo "<li class='right'><a href='../Controller/Authentification/logout.php'>Logout</a></li>
+          echo "<li class=\"right\"><a href=\"../Controller/Authentification/logout.php\">Logout</a></li>
         </ul>
     </div>";
     }
