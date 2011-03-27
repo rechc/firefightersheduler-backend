63b6789f16dc369da9757698334543089b8e9c70
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-27 18:18:54 +0000

    Klasse zum speichern der r Tabelleneintraege pro User

--- FFS/Controller/addData/AddData.php
+++ FFS/Controller/addData/AddData.php
@@ -1,7 +1,40 @@
 <?php
-    $dataselection = $_POST['auswahl'];
-    $date = $_POST['datum'];
-    $city = $_POST['ort'];
-    $participants = $_POST['check']
+//TODO WICHTIG Includes anpassen
+//require_once(PATH_BASIS . '/Model/DbConnector.php');
+//require_once('../../Model/Strecke.php');
+//require_once('../../Model/Einsatz.php');
+//require_once('../../Model/Uebung.php');
+//require_once('../../Model/Unterweisung.php');
+$date = $_POST['datum']; //??
+$city = $_POST['ort'];  //??
+$participants = $_POST['check']; //??
+
+$dataselection = $_POST['auswahl'];
+switch ($dataselection) {
+    case 'addUserToStrecke':
+        $sid = $_POST['sid'];
+        $strecke= Strecke::load($sid);
+        $uid = $_POST['uid'];
+        $strecke->add_user_connection($uid);
+        break;
+    case 'addUserToEinsatz':
+        $eid = $_POST['eid'];
+        $einsatz= Einsatz::load($einsatz);
+        $uid = $_POST['uid'];
+        $einsatz->add_user_connection($uid);
+        break;
+    case 'addUserToUebung':
+        $uebid = $_POST['uebid'];
+        $uebung= Uebung::load($uebid);
+        $uid = $_POST['uid'];
+        $uebung->add_user_connection($uid);
+        break;
+    case 'addUserToUnterweisung':
+        $uwid = $_POST['uwid'];
+        $unterweisung= Unterweisung::load($uebid);
+        $uid = $_POST['uid'];
+        $unterweisung->add_user_connection($uid);
+        break;
+}
 
 ?>
