cbb12e3bd775d80b57548ad6800895d2b812df8a
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-27 20:45:00 +0000

    Anpassungen fuer addUser to X

--- FFS/Controller/addData/AddData.php
+++ FFS/Controller/addData/AddData.php
@@ -16,28 +16,28 @@ switch ($dataselection) {
         $strecke= Strecke::load($sid);
         $uid = $_POST['uid'];
         $strecke->add_user_connection($uid);
-        header("Location: ../../View/editStrecke.php?id=".$uid);
+        header("Location: ../../View/editStrecke.php?uid=".$uid);
         break;
     case 'addUserToEinsatz':
         $eid = $_POST['eid'];
         $einsatz= Einsatz::load($eid);
         $uid = $_POST['uid'];
         $einsatz->add_user_connection($uid);
-        header("Location: ../../View/editEinsatz.php?id=".$uid);
+        header("Location: ../../View/editEinsatz.php?uid=".$uid);
         break;
     case 'addUserToUebung':
         $uebid = $_POST['uebid'];
         $uebung= Uebung::load($uebid);
         $uid = $_POST['uid'];
         $uebung->add_user_connection($uid);
-        header("Location: ../../View/editUebung.php?id=".$uid);
+        header("Location: ../../View/editUebung.php?uid=".$uid);
         break;
     case 'addUserToUnterweisung':
         $uwid = $_POST['uwid'];
         $unterweisung= Unterweisung::load($uwid);
         $uid = $_POST['uid'];
         $unterweisung->add_user_connection($uid);
-        header("Location: ../../View/editUnterweisung.php?id=".$uid);
+        header("Location: ../../View/editUnterweisung.php?uid=".$uid);
         break;
 }
 
