475d5dc37e2c7242a1362001ed5782c8bc999917
WarkenA <WarkenA@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-28 21:52:33 +0000

    id rueckgabe bei insert

--- trunk/FFS/Controller/JavaScript/jsLogin.js
+++ trunk/FFS/Controller/JavaScript/jsLogin.js
@@ -1,6 +1,6 @@
 
 function register(){
-    alert("Um sich zu registrieren wenden Sie sich bitte an den System-Administrator");
+    alert("Um sich zu registrieren wenden Sie sich bitte an den System-Administrator ihrer Feuerwehrstelle. Eine Registrierung von Dritten ist nicht erlaubt");
 }
 
 function password(){
