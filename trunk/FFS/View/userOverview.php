0de135a2144863af8a7efcc79ad75bf1607fa0ae
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-28 19:24:38 +0000

    HTML 4.01 conform

--- trunk/FFS/View/userOverview.php
+++ trunk/FFS/View/userOverview.php
@@ -14,7 +14,7 @@
             }
         ?>
         <div><h1>Benutzerübersicht</h1></div>
-        <div align="right"><a href="newUser.php">neuer Benutzer<img alt="hinzuf&uuml;gen" src="images/add.gif" /></a></div>
+        <div align="right"><a href="newUser.php">neuer Benutzer<img alt="hinzuf&uuml;gen" src="images/add.gif"></a></div>
         <table>
             <thead>
                 <tr>
@@ -29,7 +29,7 @@
                     <th>&nbsp;</th>
                 </tr>
             </thead>
-            <tfoot></tfoot>
+<!--            <tfoot></tfoot>-->
             <tbody>
                 <?php
                     $userlist = Userlist::getUserTable();
@@ -38,6 +38,11 @@
             </tbody>
         </table>
     <!-- Ende Content -->
+    <p>
+    <a href="http://validator.w3.org/check?uri=referer"><img
+        src="http://www.w3.org/Icons/valid-html401"
+        alt="Valid HTML 4.01 Transitional" height="31" width="88"></a>
+    </p>
     </div>
 <?php include (PATH_BASIS . '/View/Layout/footer.inc.php') ?>
 
