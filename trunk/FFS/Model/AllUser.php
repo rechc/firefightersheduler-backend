749a65e1da0380078f7b41638bcb064504cf5885
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-29 15:33:54 +0000

    Sortierung eingebaut

--- trunk/FFS/Model/AllUser.php
+++ trunk/FFS/Model/AllUser.php
@@ -15,7 +15,7 @@
      * @return User-ArrayObject
      */
      public static function get_userarray_for_manager_view(){
-         $sql = "SELECT ID FROM user"; // WHERE agt=1"; // TODO später fixen
+         $sql = "SELECT ID FROM user ORDER BY name, vorname"; // WHERE agt=1"; // TODO später fixen
 
          $dbConnector = DbConnector::getInstance();
          $result = $dbConnector->execute_sql($sql);
