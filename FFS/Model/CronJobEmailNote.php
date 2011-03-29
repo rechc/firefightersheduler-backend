1c34cbf3ed68f4988e6e6ff1a9ae0cf6dec09fd6
WarkenA <WarkenA@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-29 20:54:15 +0000

    email regex verbessert , conjob im model implementiert, passwort vergessen funktion auf statisch umgestellt , ausgabe von sonderzeichen in personallist

--- FFS/Model/CronJobEmailNote.php
+++ FFS/Model/CronJobEmailNote.php
@@ -2,25 +2,27 @@
 
 include_once '../global.inc.php';
 require_once(PATH_BASIS . '/Model/AllUser.php');
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  * Description of CronJobEmailNote
+ * Klasse die vom Cronjob angesprochen wird
  *
- * @author awagen
+ * @author Warken Andreas
+ * @version 1
  */
 class CronJobEmailNote {
 
-    // TODO kommentieren gesamte klasse
+    /**
+     * start
+     * Muss vom CronJob aufgerufen werden
+     * prueft alle Benutzer und versendet Emails
+     * falls die Tauglichkeit nicht in Ordnung ist   
+     */
     public static function start() {
         $all = AllUser::get_userarray_for_manager_view();
         foreach ($all as $user) {
             $user->start_cronjob_note_check();
         }
-        
     }
 
 }
