2d20191e60a936c9d4b1535327e49c5854cce5b9
tobias.lana@googlemail.com <tobias.lana@googlemail.com@8851dce3-1f88-5ac0-db65-adf6eaa01d94>
2011-03-31 08:14:37 +0000

    Include dateien bzgl Layout angepasst

--- FFS/View/Layout/navi.inc.php
+++ FFS/View/Layout/navi.inc.php
@@ -1,15 +1,19 @@
     <?php 
     include_once PATH_BASIS . '/Controller/SessionHelper.php';
-    if(SessionHelper::isLoggedIn()){
-        echo "<div id=\"buttonbar\">
+    ?>
+    <div id="buttonbar">
         <ul>
-          <li><a href=\"viewUser.php\">Meine Daten</a></li>";
+    <?php
+    if(SessionHelper::isLoggedIn()){
+        ?>
+        <li><a href="viewUser.php">Meine Daten</a></li>
+        <?php
           if(SessionHelper::isAdminOrAGW()){
             echo "<li><a href=\"userOverview.php\">Benutzerübersicht</a></li>";
             echo "<li><a href=\"addData.php\">Daten hinzuf&uuml;gen</a></li>";
           }
-          echo "<li class=\"right\"><a href=\"../Controller/Authentification/logout.php\">Logout</a></li>
-        </ul>
-        </div>";
+          echo "<li class=\"right\"><a href=\"../Controller/Authentification/logout.php\">Logout</a></li>";
     }
     ?>
+        </ul>
+    </div>
